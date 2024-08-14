5250c39c86bb70afb7038ee102a36a44a91f7f02
Kapparov.K <kapparov.K@nsk.kz>
2024-08-14 11:31:57 +0500

    test 3

--- Administration/ClientApp/src/views/Blog/BlogEdit.vue
+++ Administration/ClientApp/src/views/Blog/BlogEdit.vue
@@ -79,7 +79,7 @@
     <div class="flex items-center mb-4">
       <input id="mainpage-checkbox" type="checkbox" value="" class="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
       <label for="mainpage-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Вывести на главную под слайдером</label>
-    </div>
+    </div> 
     <div class="flex items-center">
       <input id="active-checkbox" type="checkbox" value="" class="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
       <label for="active-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Активность</label>
